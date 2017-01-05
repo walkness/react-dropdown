464dd96f5ede5e6709d76b4e29a17c5845eb6d60
Walker Angell <w@lker.co>
2017-01-04 20:41:51 -0500

    add className and right to dropdown menu

--- DropdownMenu.jsx
+++ DropdownMenu.jsx
@@ -1,14 +1,17 @@
 import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 
 
-const DropdownMenu = ({ children }) => (
-  <ul className='dropdown-menu'>
+const DropdownMenu = ({ children, className, right }) => (
+  <ul className={classNames('dropdown-menu', className, { 'dropdown-menu-right': right })}>
     { children }
   </ul>
 );
 
 DropdownMenu.propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
+  right: PropTypes.bool,
 };
 
 export default DropdownMenu;
